79c716fd43b4a04a78755d00cc5a9746c7de1134
ElsaKrasniqii <elsa.krasniqi12@student.uni-pr.edu>
2025-11-12 23:59:37 +0100

    shtimi i variablave dhe konstantave globale

--- Client_Admin/shared.h
+++ Client_Admin/shared.h
@@ -1,1 +1,15 @@
 #pragma once
+#include <string>
+#include <chrono>
+
+// --- Konstantat globale ---
+extern const std::string ADMIN_SECRET;
+extern const char* DATA_DIR;
+
+extern const int PORT;
+extern const int TIMEOUT_SECONDS;
+
+#define BUFFER_SIZE 1024
+
+// --- Variabla globale ---
+extern bool isAuthenticated; 
--- Client_readonly/shared.h
+++ Client_readonly/shared.h
@@ -1,1 +1,16 @@
 #pragma once
+#pragma once
+#include <string>
+#include <chrono>
+
+// --- Konstantat globale ---
+extern const std::string ADMIN_SECRET;
+extern const char* DATA_DIR;
+
+extern const int PORT;
+extern const int TIMEOUT_SECONDS;
+
+#define BUFFER_SIZE 1024
+
+// --- Variabla globale ---
+extern bool isAuthenticated;
