c9614fa02c7f891dbe5a3184356560038f3c5d0f
ElonaKuqiiii <elona.kuqi@student.uni-pr.edu>
2025-11-13 12:11:59 +0100

    implementimi i funksionit uploadFile

--- Client_readonly/shared.cpp
+++ Client_readonly/shared.cpp
@@ -79,3 +79,10 @@ string infoFile(const string& filename) {
         << put_time(&timeinfo, "%Y-%m-%d %H:%M:%S") << "\n";
     return ss.str();
 }
+std::string uploadFile(const std::string& name, const std::string& content) {
+    std::ofstream file(DATA_DIR + std::string("/") + name);
+    if (!file) return "Gabim: nuk mund te krijohet fajlli.";
+
+    file << content;
+    return "Fajlli u ruajt me sukses.";
+}
