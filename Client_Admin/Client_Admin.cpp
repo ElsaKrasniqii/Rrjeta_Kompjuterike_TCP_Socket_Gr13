cdd612bf018bc8862819c9eb299df3fe15219d14
Florjetë Kuka <59765295+florjete@users.noreply.github.com>
2025-11-16 12:57:04 +0100

    shtimi i funksionalitetit download

--- Client_Admin/Client_Admin.cpp
+++ Client_Admin/Client_Admin.cpp
@@ -13,123 +13,140 @@
 
 using namespace std;
 
-const char* SERVER_IP = "127.0.0.1";   // IP e serverit
+const char* SERVER_IP = "127.0.0.1";
 
 int main() {
+
     WSADATA wsa;
     SOCKET s = INVALID_SOCKET;
     sockaddr_in srv{};
     char buffer[BUFFER_SIZE];
 
-    // Inicializimi i Windows Socket
+    // Initialize Winsock
     if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
-        cout << " WSAStartup failed\n";
+        cout << "WSAStartup failed\n";
         return 1;
     }
 
-	// Krijimi i socket-it UDP
+    // Create UDP socket
     s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if (s == INVALID_SOCKET) {
-        cout << " Socket creation failed. Error: " << WSAGetLastError() << "\n";
-        WSACleanup();
+        cout << "Socket creation failed\n";
         return 1;
     }
 
-    // Konfigurimi i adreses se serverit
+    // Configure server address
     srv.sin_family = AF_INET;
-    int pton = inet_pton(AF_INET, SERVER_IP, &srv.sin_addr);
-    if (pton != 1) {
-        srv.sin_addr.s_addr = inet_addr(SERVER_IP);
-    }
+    inet_pton(AF_INET, SERVER_IP, &srv.sin_addr);
     srv.sin_port = htons(PORT);
 
-    cout << " Admin client (UDP)\n";
-    cout << " Server: " << SERVER_IP << ":" << PORT << "\n";
-    cout << " Fillimisht jep /auth " << ADMIN_SECRET << " per privilegje.\n";
-    cout << " Perdor komanden /uploadfile <path> per te ngarkuar nje file lokal.\n";
+    cout << "Admin UDP client.\n";
+    cout << "Use /auth " << ADMIN_SECRET << "\n";
+    cout << "Use /uploadfile <path>\n";
+    cout << "Use /download <filename>\n";
 
     while (true) {
         cout << "\n> ";
         string line;
-        if (!getline(cin, line)) break;
-        if (line == "exit" || line == "quit") break;
+        getline(cin, line);
+
+        if (line == "exit") break;
         if (line.empty()) continue;
 
-        //  Kontrollon nese komanda eshte /uploadfile <path>
-        const string uploadFilePrefix = "/uploadfile ";
-        if (line.rfind(uploadFilePrefix, 0) == 0) { 
-            string localPath = line.substr(uploadFilePrefix.size());
+        // Upload file
+        const string uploadPrefix = "/uploadfile ";
+        if (line.rfind(uploadPrefix, 0) == 0) {
 
-            while (!localPath.empty() && isspace((unsigned char)localPath.front())) localPath.erase(0, 1);
-            while (!localPath.empty() && isspace((unsigned char)localPath.back())) localPath.pop_back();
+            string localPath = line.substr(uploadPrefix.size());
 
-            // Lexo permbajtjen e file-it lokal
+            // Trim spaces
+            while (!localPath.empty() && isspace((unsigned char)localPath.front()))
+                localPath.erase(0, 1);
+
+            while (!localPath.empty() && isspace((unsigned char)localPath.back()))
+                localPath.pop_back();
+
+            // Open local file
             ifstream ifs(localPath, ios::binary);
             if (!ifs) {
-                cout << " Gabim: nuk u gjet file-i lokal: " << localPath << "\n";
+                cout << "File not found: " << localPath << "\n";
                 continue;
             }
 
+            // Read file content
             string content((istreambuf_iterator<char>(ifs)), istreambuf_iterator<char>());
-            ifs.close();
 
-            // Nxjerr vetem emrin e file-it (pa path)
-            size_t pos1 = localPath.find_last_of('/');
-            size_t pos2 = localPath.find_last_of('\\');
-            size_t pos = string::npos;
-            if (pos1 != string::npos && pos2 != string::npos) pos = max(pos1, pos2);
-            else if (pos1 != string::npos) pos = pos1;
-            else if (pos2 != string::npos) pos = pos2;
-
+            // Extract filename
+            size_t pos = localPath.find_last_of("/\\");
             string filename = (pos == string::npos) ? localPath : localPath.substr(pos + 1);
 
-            // Krijon komanden per serverin ne format te caktuar
+            // Build upload command
             string uploadCmd = "/upload " + filename + "|" + content;
 
             if (uploadCmd.size() > BUFFER_SIZE) {
-                cout << " File-i eshte shume i madh (" << uploadCmd.size()
-                    << " byte). Rrit BUFFER_SIZE ose ndaje ne pjese me te vogla.\n";
+                cout << "File too large.\n";
                 continue;
             }
 
-            line = uploadCmd; 
+            line = uploadCmd;
         }
 
-        // Dergon komanden te serveri
-        sendto(
-            s,
-            line.c_str(),
-            static_cast<int>(line.size()),
-            0,
-            reinterpret_cast<sockaddr*>(&srv),
-            sizeof(srv)
-        );
+        // Download file
+        const string dlPrefix = "/download ";
+        bool isDownload = false;
+        string dlFilename;
 
-        // Merr pergjigjen nga serveri
+        if (line.rfind(dlPrefix, 0) == 0) {
+            dlFilename = line.substr(dlPrefix.size());
+            isDownload = true;
+        }
+
+        // Send command to server
+        sendto(s, line.c_str(), static_cast<int>(line.size()), 0, (sockaddr*)&srv, sizeof(srv));
+
+        // Receive response
         sockaddr_in sender{};
-        int senderLen = sizeof(sender);
+        int sl = sizeof(sender);
         memset(buffer, 0, BUFFER_SIZE);
 
-        int got = recvfrom(
-            s,
-            buffer,
-            BUFFER_SIZE,
-            0,
-            reinterpret_cast<sockaddr*>(&sender),
-            &senderLen
-        );
+        int got = recvfrom(s, buffer, BUFFER_SIZE, 0, (sockaddr*)&sender, &sl);
+        if (got <= 0) {
+            cout << "(No response)\n";
+            continue;
+        }
 
-        if (got > 0) {
-            cout << string(buffer, buffer + got) << "\n";
+        string response(buffer, got);
+
+        // If this is a download, save file locally
+        if (isDownload) {
+
+            if (response.rfind("ERROR", 0) == 0) {
+                cout << response << "\n";
+                continue;
+            }
+
+            // Save as downloaded_<filename>
+            string outPath = "downloaded_" + dlFilename;
+            ofstream ofs(outPath, ios::binary);
+
+            if (!ofs) {
+                cout << "Failed to save file locally.\n";
+            }
+            else {
+                ofs << response;
+                ofs.close();
+                cout << "File downloaded and saved as: " << outPath << "\n";
+            }
+
+            continue;
         }
-        else {
-            cout << "( Nuk erdhi pergjigje nga serveri)\n";
-        }
+
+        // Normal server response
+        cout << response << "\n";
     }
 
-	// Mbyllja e socket-it
+    // Close socket
     closesocket(s);
     WSACleanup();
-    cout << " Klienti u mbyll me sukses.\n";
     return 0;
 }
